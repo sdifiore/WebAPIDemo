c3b0beaf854874b9ee25e00120cc5b7968f02614
Sergio Di Fiore <difiores@outlook.com>
2025-06-19 09:50:24 -0300

    Add project files.

---
+++ WebAPIDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+// Routing
+
+app.MapGet("/shirts", () =>
+{
+    return "Reading all the shirts";
+});
+
+app.MapGet("/shirts/{id}", (int id) =>
+{
+    return $"Reading shirt with ID: {id}";
+});
+
+app.MapPost("/shirts", (string shirt) =>
+{
+    return $"Creating a new shirt: {shirt}";
+});
+
+app.MapPut("/shirts/{id}", (int id) =>
+{
+    return $"Updating shirt with ID: {id}";
+});
+
+app.MapDelete("/shirts/{id}", (int id) =>
+{
+    return $"Deleting shirt with ID: {id}";
+});
+
+app.Run();
