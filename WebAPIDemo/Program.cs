8109cb99a472b298848449b72b65639a6ec4f836
Sergio Di Fiore <difiores@outlook.com>
2025-06-20 11:39:39 -0300

    Basic controller

--- WebAPIDemo/Program.cs
+++ WebAPIDemo/Program.cs
@@ -1,41 +1,14 @@
 ﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
 
-// Routing
-
-app.MapGet("/shirts", () =>
-{
-    return "Reading all the shirts";
-});
-
-app.MapGet("/shirts/{id}", (int id) =>
-{
-    return $"Reading shirt with ID: {id}";
-});
-
-app.MapPost("/shirts", () =>
-{
-    return $"Creating a new shirt.";
-});
-
-app.MapPut("/shirts/{id}", (int id) =>
-{
-    return $"Updating shirt with ID: {id}";
-});
-
-app.MapDelete("/shirts/{id}", (int id) =>
-{
-    return $"Deleting shirt with ID: {id}";
-});
+app.MapControllers();
 
 app.Run();
--- WebAPIDemo/ShirtsController.cs
+++ WebAPIDemo/ShirtsController.cs
@@ -5,26 +5,36 @@ namespace WebAPIDemo
     [ApiController]
     public class ShirtsController : ControllerBase
     {
+        [HttpGet]
+        [Route("/shirts")]
         public string GetShirt()
         {
             return "Reading all the shirt";
         }
 
+        [HttpGet]
+        [Route("/shirts/{id}")]
         public string GetShirtById(int id)
         {
             return $"Reading shirt with ID: {id}";
         }
 
+        [HttpPost]
+        [Route("/shirts")]
         public string CreateShirt()
         {
             return "Creating a new shirt.";
         }
 
+        [HttpPut]
+        [Route("/shirts/{id}")]
         public string UpdateShirt(int id)
         {
             return $"Updating shirt with ID: {id}";
         }
 
+        [HttpDelete]
+        [Route("/shirts/{id}")]
         public string DeleteShirt(int id)
         {
             return $"Deleting shirt with ID: {id}";
