372d5132bec7b56ad9295723abe868f0906cd4a4
Sergio Di Fiore <difiores@outlook.com>
2025-06-22 10:28:56 -0300

    Shirt_EnsureCorrectSizingAttribute not working

--- WebAPIDemo/Models/Shirt.cs
+++ WebAPIDemo/Models/Shirt.cs
@@ -1,13 +1,26 @@
-﻿namespace WebAPIDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+using WebAPIDemo.Models.Validations;
+
+namespace WebAPIDemo.Models
 {
     public class Shirt
     {
         public int ShirtId { get; set; }
+
+        [Required(ErrorMessage = "Shirt name is required.")]
         public string? Brand { get; set; }
+
+        [Required(ErrorMessage = "Shirt color is required.")]
         public string? Color { get; set; }
-        public int Size { get; set; }
+
+        [Shirt_EnsureCorrectSizing]
+        public int? Size { get; set; }
+
+        [Required]
         public string? Gender { get; set; }
-        public double Price { get; set; }
+
+        public double? Price { get; set; }
         // Additional properties can be added as needed
     }
 }
---
+++ WebAPIDemo/Models/Validations/Shirt_EnsureCorrectSizingAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIDemo.Models.Validations
+{
+    public class Shirt_EnsureCorrectSizingAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var shirt = validationContext.ObjectInstance as Shirt;
+
+            if (shirt != null && !string.IsNullOrWhiteSpace(shirt.Gender))
+            {
+                if (shirt.Gender.Equals("men", StringComparison.OrdinalIgnoreCase) && (shirt.Size < 8))
+                    return new ValidationResult("For men's shirt the Size must be greater or equal than 8.");
+                else if (shirt.Gender.Equals("women", StringComparison.OrdinalIgnoreCase) && (shirt.Size < 6))
+                    return new ValidationResult("For women's shirt the Size must be greater or equal than 8.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
